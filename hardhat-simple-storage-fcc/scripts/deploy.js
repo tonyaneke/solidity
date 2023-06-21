b4d5ce7d6b1061d02d8da55c507a04e671cb99ba
tonyaneke <77333061+tonyaneke@users.noreply.github.com>
2023-06-21 14:02:26 +0100

    Add files via upload

---
+++ hardhat-simple-storage-fcc/scripts/deploy.js
@@ -0,0 +1,19 @@
+// imports 
+const { ethers } = require("hardhat")
+
+// async main
+async function main() {
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+  console.log("Deploying contract...")
+  const simpleStorage = await SimpleStorageFactory.deployContract();
+  await simpleStorage.waitForDeployment();
+  // console.log(`Deployed contract to: ${simpleStorage.address}`)
+}
+
+// main
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+  console.error(error);
+  process.exit(1);
+})
